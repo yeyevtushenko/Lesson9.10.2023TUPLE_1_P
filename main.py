007bbb8a94531b37d13a857b99086c96bebe641c
yeyevtushenko <yehor803@gmail.com>
2024-01-29 11:32:18 +0200

    Завдання 7 Маємо дві множини з назвами міст. Створіть третю множину з тими назвами, які є в обох множинах.

---
+++ main.py
@@ -0,0 +1,7 @@
+cities_set1 = {"Kyiv", "Lviv", "Odessa", "Kharkiv"}
+cities_set2 = {"Lviv", "Dnipro", "Odessa", "Kherson"}
+
+
+common_cities_set = cities_set1.intersection(cities_set2)
+
+print("Спільні міста у обох множинах:", common_cities_set)
